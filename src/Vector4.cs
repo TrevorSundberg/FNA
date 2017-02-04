2238e2bf74476d0721e986002e505cc5f9a0794b
TrevorSundberg <Trevor.Sundberg@gmail.com>
2017-02-04 10:42:47 -0800

    Revert "Vector exact float equality"
    This reverts commit e3869e23cbcc84f3ca9177606c8f482dc21efb6c.

--- src/Vector4.cs
+++ src/Vector4.cs
@@ -228,11 +228,10 @@ namespace Microsoft.Xna.Framework
 		/// <returns><c>true</c> if the instances are equal; <c>false</c> otherwise.</returns>
 		public bool Equals(Vector4 other)
 		{
-			return
-				X == other.X &&
-				Y == other.Y &&
-				Z == other.Z &&
-				W == other.W;
+			return (	(MathHelper.WithinEpsilon(this.W, other.W)) &&
+					(MathHelper.WithinEpsilon(this.X, other.X)) &&
+					(MathHelper.WithinEpsilon(this.Y, other.Y)) &&
+					(MathHelper.WithinEpsilon(this.Z, other.Z))	);
 		}
 
 		/// <summary>
@@ -1383,11 +1382,10 @@ namespace Microsoft.Xna.Framework
 
 		public static bool operator ==(Vector4 value1, Vector4 value2)
 		{
-			return
-				value1.X == value2.X &&
-				value1.Y == value2.Y &&
-				value1.Z == value2.Z &&
-				value1.W == value2.W;
+			return (	(MathHelper.WithinEpsilon(value1.W, value2.W)) &&
+					(MathHelper.WithinEpsilon(value1.X, value2.X)) &&
+					(MathHelper.WithinEpsilon(value1.Y, value2.Y)) &&
+					(MathHelper.WithinEpsilon(value1.Z, value2.Z))	);
 		}
 
 		public static bool operator !=(Vector4 value1, Vector4 value2)
